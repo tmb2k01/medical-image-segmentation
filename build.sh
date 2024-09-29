f2369427415017288b4be36fbd7ba6e3949fc03f
pasztork <pasztor.kristof.kp@gmail.com>
2024-09-29 12:32:27 +0200

    Remove images with same name

--- build.sh
+++ build.sh
@@ -7,6 +7,12 @@ if [ "$1" ]; then
     TAG="$1"
 fi
 
+EXISTING_IMAGE=$(docker images -q "$IMAGE_NAME:$TAG")
+if [ -n "$EXISTING_IMAGE" ]; then
+    echo "Removing Docker image: $IMAGE_NAME:$TAG"
+    docker rmi -f "$IMAGE_NAME:$TAG" 2>/dev/null
+fi
+
 echo "Building Docker image: $IMAGE_NAME:$TAG"
 docker build -t "$IMAGE_NAME:$TAG" .
 
