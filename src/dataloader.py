c69614c6ac03a6b63f85a9bd3b47da125abd2a07
tmb2k01 <tmb2k01@gmail.com>
2024-11-04 19:09:09 +0100

    create basic ensemble model

--- src/dataloader.py
+++ src/dataloader.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
-
+from torchvision import transforms
 from util import is_power_of_two
 
 root_dir = Path(__file__).parent.parent
@@ -69,17 +69,28 @@ class BrainTumourDataset(Dataset):
 
         flair = data[:, :, depth_start:depth_end, 0]
         t1w = data[:, :, depth_start:depth_end, 1]
+        t1gd = data[:, :, depth_start:depth_end, 2]
+        t2w = data[:, :, depth_start:depth_end, 3]
 
         # Resize each slice properly to maintain the dimensions
         flair_resized = cv2.resize(flair, self.dim)
         t1w_resized = cv2.resize(t1w, self.dim)
+        t1gd_resized = cv2.resize(t1gd, self.dim)
+        t2w_resized = cv2.resize(t2w, self.dim)
+
         seg_resized = cv2.resize(seg, self.dim, interpolation=cv2.INTER_NEAREST)
 
-        X = np.stack((flair_resized, t1w_resized), axis=-1)
+        X = np.stack((flair_resized, t1w_resized, t1gd_resized, t2w_resized), axis=-1)
+
+        # Standard normalization (mean and std)
+        X_mean = np.mean(X, axis=(0, 1, 2))
+        X_std = np.std(X, axis=(0, 1, 2))
+        X_normalized = (X - X_mean) / (X_std + 1e-8)
+
         y = seg_resized[:, :, depth_start:depth_end]
 
         # Normalize and convert to tensors
-        X_tensor = torch.from_numpy(X).permute(3, 0, 1, 2).float() / np.max(X)
+        X_tensor = torch.from_numpy(X_normalized).permute(3, 0, 1, 2).float()
         y_tensor = torch.from_numpy(y).long()
         Y = F.one_hot(y_tensor, num_classes=4).permute(3, 0, 1, 2).float()
 
@@ -94,7 +105,6 @@ class BrainTumourDataModule(pl.LightningDataModule):
         batch_size=1,
         num_workers=4,
         img_dim=(IMG_SIZE, IMG_SIZE),
-        transform=None,
     ):
         super().__init__()
         self.data_path = data_path
@@ -102,7 +112,13 @@ class BrainTumourDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.img_dim = img_dim
-        self.transform = transform
+        self.train_transform = transforms.Compose(
+            [
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.ToTensor(),
+            ]
+        )
         self.train_ids = self.val_ids = self.test_ids = None
 
     def prepare_data(self):
@@ -124,21 +140,19 @@ class BrainTumourDataModule(pl.LightningDataModule):
             self.seg_path,
             self.train_ids,
             img_dim=self.img_dim,
-            transform=self.transform,
+            transform=self.train_transform,
         )
         self.val_dataset = BrainTumourDataset(
             self.data_path,
             self.seg_path,
             self.val_ids,
             img_dim=self.img_dim,
-            transform=self.transform,
         )
         self.test_dataset = BrainTumourDataset(
             self.data_path,
             self.seg_path,
             self.test_ids,
             img_dim=self.img_dim,
-            transform=self.transform,
         )
 
     def train_dataloader(self):
